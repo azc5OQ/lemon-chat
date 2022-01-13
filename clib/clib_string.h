5f5d1cdad0afc79d3834cb17d165a0b2f4424b40
taxodium <91157367+taxodium@users.noreply.github.com>
2022-01-12 18:14:26 -0800

    Update clib_string.h

--- clib/clib_string.h
+++ clib/clib_string.h
@@ -2,9 +2,17 @@
 
 #define CLIB_STRING_H
 
-#include <stdbool.h>
+#ifndef TRUE
+typedef int BOOL;
+#define TRUE 1
+#endif
 
-bool clib__is_str_number(char* string_to_check);
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+BOOL clib__is_str_number(char* string_to_check);
 void clib__int_to_hex_string(int n, char* hex_string);
+void clib__sanitize_stdin(char* input);
 
 #endif
