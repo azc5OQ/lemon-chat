dc8c3efc3a67a656debdfaa051d6f7008e83fc94
azc5OQ <91157367+azc5OQ@users.noreply.github.com>
2022-09-06 13:36:08 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,1 +0,0 @@
-gcc -Wall -fstack-protector-all -D_FORTIFY_SOURCE=2 main.c clib/clib_string.c clib/clib_memory.c kokke-tiny-aes-c/aes.c zhicheng/base64.c theldus-websocket/base64.c theldus-websocket/handshake.c theldus-websocket/sha1.c theldus-websocket/ws.c dave-g-json/cJSON.c ITH-sha/sha256.c -lpthread -o server.exe
